1be6447c8dd2a67dadc2cf374b30dc6dbac35c79
Jibran Ibrahim Syed <jisyed14@gmail.com>
2014-08-23 17:11:59 -0500

    Trying to make a spawn point object with gizmos

---
+++ LD30Project/Assets/Scripts/LevelSystem/SpawnPoint.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour 
+{
+	public Color zoneColor; 
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Debug.Log("Bam!");
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		// Draw transparent zone
+		float originalRed = zoneColor.r;
+		float originalGreen = zoneColor.g;
+		float originalBlue = zoneColor.b;
+		zoneColor.r = originalRed;
+		zoneColor.g = originalGreen;
+		zoneColor.b = originalBlue;
+		zoneColor.a = 0.5f;
+		Gizmos.color = zoneColor;
+		Gizmos.DrawCube(transform.position, transform.localScale);
+
+		// Draw outline
+		zoneColor.a = 1.0f;
+		Gizmos.color = zoneColor;
+		Gizmos.DrawWireCube(transform.position, transform.localScale);
+
+		// Draw spawnpoint center
+		zoneColor.r = 1.0f;
+		zoneColor.g = 0.1f;
+		zoneColor.b = 0.5f;
+		Gizmos.color = zoneColor;
+		Gizmos.DrawSphere(transform.position, 0.15f);
+		zoneColor.r = originalRed;
+		zoneColor.g = originalGreen;
+		zoneColor.b = originalBlue;
+	}
+}
