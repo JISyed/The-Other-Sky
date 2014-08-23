3fea9896ed8b32fecf2877c6b355ef74d28acb33
Jibran Ibrahim Syed <jisyed14@gmail.com>
2014-08-23 17:24:54 -0500

    Added death zones and made FadeInners prefabs

---
+++ LD30Project/Assets/Scripts/LevelSystem/DeathZone.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour 
+{
+	public Color zoneColor; 
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Debug.Log("Bleh!");
+		}
+	}
+	
+	void OnDrawGizmos()
+	{
+		// Draw transparent zone
+		zoneColor.a = 0.5f;
+		Gizmos.color = zoneColor;
+		Gizmos.DrawCube(transform.position, transform.localScale);
+		
+		// Draw outline
+		zoneColor.a = 1.0f;
+		Gizmos.color = zoneColor;
+		Gizmos.DrawWireCube(transform.position, transform.localScale);
+	}
+}
